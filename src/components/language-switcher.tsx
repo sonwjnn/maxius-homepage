0dd2ac81e9a1ff04b04ef76e647c7d35b28ddecf
sonwjnn <hoangsonj4f@gmail.com>
2025-01-16 01:50:05 +0700

    color when sheet opened

--- src/components/language-switcher.tsx
+++ src/components/language-switcher.tsx
@@ -2,10 +2,12 @@
 
 import { cn, getCorlorNavByIndex } from "@/lib/utils";
 import { useFullpageIndex } from "@/store/use-fullpage-index";
+import { useNavSheet } from "@/store/use-nav-sheet";
 import { useParams, useRouter } from "next/navigation";
 
 export const LanguageSwitcher = () => {
   const [index] = useFullpageIndex();
+  const [open] = useNavSheet();
   const params = useParams();
   const lang = params.locale || "en";
   const router = useRouter();
@@ -20,7 +22,8 @@ export const LanguageSwitcher = () => {
     <div
       className={cn(
         "flex gap-x-2 text-[21px] font-light z-50 pointer-events-auto fixed right-[50px] lg:right-[90px] top-20 lg:top-[100px]",
-        color
+        color,
+        open && "text-white"
       )}
     >
       <button
